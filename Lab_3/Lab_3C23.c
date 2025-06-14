737fa46ed6e3b562bad1bdc481f976855d621010
Narola Yash <yashnarola2525@gmail.com>
2025-06-14 10:59:19 +0530

    Lab 3 Completed

--- Lab_3/Lab_3C23.c
+++ Lab_3/Lab_3C23.c
@@ -4,6 +4,42 @@
 #include <string.h>
 
 int main() {
-    
+    int n;
+    printf("Enter number how many names you want to enter : ");
+    scanf("%d", &n);
+
+    // make char ary
+    char names[n][20], temp[20];
+
+    // input names
+    printf("--- Input Names --- \n");
+    for(int i=0; i<n; i++) {
+        printf("Entre name %d : " , (i+1));
+        scanf("%s", names[i]);
+    }
+
+    // print before sorting in alphabetical order
+    printf("--- print all names before sortnig  --- \n");
+    for(int i=0; i<n; i++) {
+        printf("%d Name : %s \n", (i+1), names[i]);
+    }
+
+    // logic for sorting names
+    for(int i=0; i<(n-1); i++) {
+        for(int j=(i+1); j<n; j++) {
+            if(strcmp(names[i], names[j]) > 0) {
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+
+    // print after sorting in alphabetical order
+    printf("--- print all names after sortnig  --- \n");
+    for(int i=0; i<n; i++) {
+        printf("%d Name : %s \n", (i+1), names[i]);
+    }
+
     return 0;
 }
