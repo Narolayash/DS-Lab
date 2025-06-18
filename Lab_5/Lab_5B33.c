bafef37161ef440bd45b212e58e0ebedc905e57f
Narola Yash <yashnarola2525@gmail.com>
2025-06-18 22:15:01 +0530

    Lab_5 completed

--- Lab_5/Lab_5B33.c
+++ Lab_5/Lab_5B33.c
@@ -13,6 +13,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+void anagarm(char str1[20], char str2[20]);
+
 int main() {
     // create char array
     int n;
@@ -37,7 +39,36 @@ int main() {
     printf("Enter Your String : ");
     scanf("%s", userStr);
 
-    // baki
+    anagarm(words[randomNum], userStr);
 
     return 0;
+}
+
+// check anagram or not
+void anagarm(char str1[20], char str2[20]) {
+    for(int i=0; i<str1[i]!='\0'; i++) {
+        for(int j=0; j<str1[j]!='\0'; j++) {
+            if(str1[i] < str1[j]) {
+                char temp = str1[i];
+                str1[i] = str1[j];
+                str1[j] = temp;
+            }
+        }
+    }
+    for(int i=0; i<str2[i]!='\0'; i++) {
+        for(int j=0; j<str2[j]!='\0'; j++) {
+            if(str2[i] < str2[j]) {
+                char temp = str2[i];
+                str2[i] = str2[j];
+                str2[j] = temp;
+            }
+        }
+    }
+
+    if (strcmp(str1, str2) == 0) {
+        printf("Strings are equal\n");
+    } else {
+        printf("Strings are not equal\n");
+    }
+
 }
